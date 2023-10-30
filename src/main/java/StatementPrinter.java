0076f4e0c3c864b6ab0a4e6469ac4ed774bd9a36
flkmhd <jm_falek@esi.dz>
2023-10-30 20:17:38 +0100

    ajout de stringBuilder et la gestion des sommes flottantes

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -4,29 +4,28 @@ import java.util.*;
 public class StatementPrinter {
 
   public String print(Invoice invoice, HashMap<String, Play> plays) {
-    int totalAmount = 0;
+    double totalAmount = 0.0;
     int volumeCredits = 0;
-    String result = String.format("Statement for %s\n", invoice.customer);
+    StringBuilder result = new StringBuilder();
+    result.append(String.format("Statement for %s\n", invoice.customer));
 
-    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
+    NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
-
+      double thisAmount = 0.0;
       switch (play.type) {
         case "tragedy":
-          thisAmount = 40000;
+          thisAmount = 400.00; // Montant de base pour une tragédie en dollars avec des centimes
           if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            thisAmount += 10.00 * (perf.audience - 30); // Ajouter 10 dollars par siège au-delà de 30
           }
           break;
         case "comedy":
-          thisAmount = 30000;
+          thisAmount = 300.00; // Montant de base pour une comédie en dollars avec des centimes
           if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            thisAmount += 100.00 + 5.00 * (perf.audience - 20); // Ajouter 100 dollars plus 5 dollars par siège au-delà de 20
           }
-          thisAmount += 300 * perf.audience;
+          thisAmount += 3.00 * perf.audience; // Ajouter 3 dollars par siège
           break;
         default:
           throw new Error("unknown type: ${play.type}");
@@ -37,13 +36,15 @@ public class StatementPrinter {
       // add extra credit for every ten comedy attendees
       if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
-      // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
-      totalAmount += thisAmount;
+      // Ajouter une ligne au résultat avec le nom de la pièce, le montant et le nombre de sièges
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, currencyFormatter.format(thisAmount), perf.audience));
+      totalAmount += thisAmount; // Accumuler le montant total
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
+    // Ajouter le montant total dû et les crédits gagnés au résultat
+    result.append(String.format("Amount owed is %s\n", currencyFormatter.format(totalAmount)));
+    result.append(String.format("You earned %s credits\n", volumeCredits));
+
+    return result.toString(); // Retourner le résultat complet sous forme de chaîne;
   }
 
 }
