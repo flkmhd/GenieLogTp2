89416771d7b9005b9a7905c471062c855dc6641f
flkmhd <jm_falek@esi.dz>
2023-10-30 20:36:58 +0100

    l'ajout des tests unitaires

--- src/test/java/StatementPrinterTests.java
+++ src/test/java/StatementPrinterTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +27,39 @@ public class StatementPrinterTests {
         var result = statementPrinter.print(invoice, plays);
 
         verify(result);
+    }
+    @Test
+    void testAudience30ForTragedy() {
+        HashMap<String, Play> plays = new HashMap<>();
+        plays.put("playID", Play.createPlay("Play Name", "tragedy"));
+
+        Invoice invoice = new Invoice("Customer Name", List.of(new Performance("playID", 30)));
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.print(invoice, plays);
+
+        String expectedStatement = "Statement for Customer Name\n" +
+                "  Play Name: $400.00 (30 seats)\n" +
+                "Amount owed is $400.00\n" +
+                "You earned 0 credits\n";
+
+        assertEquals(expectedStatement, result);
+    }
+    @Test
+    void testAudience20ForComedy() {
+        HashMap<String, Play> plays = new HashMap<>();
+        plays.put("playID", Play.createPlay("Play Name", "comedy"));
+
+        Invoice invoice = new Invoice("Customer Name", List.of(new Performance("playID", 20)));
+
+        StatementPrinter statementPrinter = new StatementPrinter();
+        var result = statementPrinter.print(invoice, plays);
+
+        String expectedStatement = "Statement for Customer Name\n" +
+                "  Play Name: $360.00 (20 seats)\n" +
+                "Amount owed is $360.00\n" +
+                "You earned 4 credits\n"; // Mise à jour de cette ligne en fonction du comportement attendu
+        assertEquals(expectedStatement, result);
     }
     /*@Test
     void statementWithNewPlayTypes() {
